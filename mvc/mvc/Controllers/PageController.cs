144e9dd5ce7d2a513ac56adcda386d80c0cd4539
karenovnameriko <meri.terteryan.2007@mail.ru>
2026-03-05 12:25:19 +0300

    Add KT1

---
+++ mvc/mvc/Controllers/PageController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc.Controllers
+{
+	public class PageController : Controller
+	{
+        public IActionResult Welcome()
+        {
+            return View();
+
+        }
+
+        // Задание 2
+
+        [Route("Page/Greet/{name?}")]
+        public IActionResult Greet(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Гость";
+            }
+            ViewBag.Name = name;
+            return View();
+        }
+
+
+
+        // Задание 3
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            ViewBag.Message = "Пусто";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                message = "Вы ничего не ввели";
+            }
+
+            ViewBag.Message = message;
+            return View("EditResult");
+        }
+
+        
+        // Задание 4
+        
+    }
+}
+
---
+++ mvc/mvc/README.md
@@ -0,0 +1,37 @@
+# Задание 1
+![img.png](img.png)
+
+
+![img_1.png](img_1.png)
+
+![img_2.png](img_2.png)
+
+# Задание 2
+
+![img_3.png](img_3.png)
+
+![img_4.png](img_4.png)
+
+![img_5.png](img_5.png)
+
+# Задание 3
+
+![img_6.png](img_6.png)
+
+![img_7.png](img_7.png)
+
+![img_8.png](img_8.png)
+
+![img_9.png](img_9.png)
+
+# Задание 4
+
+![img_10.png](img_10.png)
+
+![img_11.png](img_11.png)
+
+![img_12.png](img_12.png)
+
+![img_13.png](img_13.png)
+
+![img_14.png](img_14.png)
